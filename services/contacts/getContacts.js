0bedbc118ed095f676e034adcfae746047007ee4
Vladimir <Vladimir.Stognij@gmail.com>
2022-12-29 12:57:36 +0200

    ...

--- services/contacts/getContacts.js
+++ services/contacts/getContacts.js
@@ -4,8 +4,6 @@ const { Contact } = require("../../models/contactModel");
 const getContacts = async ({ owner, favorite }, { skip, limit }) => {
   const filter = !favorite ? { owner } : { owner, favorite: favorite };
 
-  console.log("favotite :>> ", favorite, typeof favorite);
-
   const contacts = await Contact.find(filter)
     .select({ __v: 0 })
     .skip(skip)
